0d10541b5dfeef6dec8ae930f5ae934438b54c92
SviatoslavExpert <s.granovskiy@gmail.com>
2018-02-18 17:33:47 +0200

    task0303

--- src/level_03/task0303.java
+++ src/level_03/task0303.java
@@ -10,8 +10,8 @@ package level_03;
 
 public class task0303 {
     public static void main(String[] args) {
-        System.out.println(convertEurToUsd(1, 2));
-        System.out.println(convertEurToUsd(3, 3));
+        System.out.println(convertEurToUsd(1, 1.3));
+        System.out.println(convertEurToUsd(3, 1.5));
     }
     public static double convertEurToUsd(int eur, double course){
         return eur * course;
