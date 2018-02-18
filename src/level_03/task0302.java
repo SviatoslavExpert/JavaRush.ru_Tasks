aebc7e854fd66fdc1d01599fc8ddba503a454506
SviatoslavExpert <s.granovskiy@gmail.com>
2018-02-18 17:25:35 +0200

    task0302

---
+++ src/level_03/task0302.java
@@ -0,0 +1,16 @@
+/*
+Немедленно в печать
+Добавь метод public static void printString(String s), который будет выводить переданную строку на экран.
+ */
+
+package level_03;
+
+public class task0302 {
+    public static void printString (String s){
+        System.out.println(s);
+    }
+    public static void main(String[] args) {
+        printString("Hello Amigo!");
+    }
+}
+
