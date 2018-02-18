8f64f03cfa2caa91cfb9433ec6000f495525284d
SviatoslavExpert <s.granovskiy@gmail.com>
2018-02-18 15:48:58 +0200

    task0214

---
+++ src/level_02/task0214.java
@@ -0,0 +1,24 @@
+/*
+Минимум двух чисел
+Написать функцию, которая возвращает минимум из двух чисел.
+Подсказка:
+Нужно написать тело существующей функции min.
+ */
+
+package level_02;
+
+public class task0214 {
+    public static int min(int a, int b)
+    {
+        if(a < b)
+            return a;
+        else
+            return b;
+    }
+    public static void main(String[] args) throws Exception
+    {
+        System.out.println( min(12,33) );
+        System.out.println( min(-20,0) );
+        System.out.println( min(-10,-20) );
+    }
+}
