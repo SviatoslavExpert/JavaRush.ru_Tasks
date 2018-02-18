095ff3d95d2d32b750fabd269436982b70ce0fda
SviatoslavExpert <s.granovskiy@gmail.com>
2018-02-18 21:25:08 +0200

    task0302

--- src/level_03/task0302.java
+++ src/level_03/task0302.java
@@ -1,20 +1,16 @@
 /*
-Обмен валют
-Реализуй метод convertEurToUsd, переводящий евро в доллары по заданному курсу.
-Вызови его дважды в методе main с любыми параметрами. Результаты выведи на экран, каждый раз с новой строки.
-Подсказка:
-Расчет выполняется по формуле: долларСША = евро * курс
+Немедленно в печать
+Добавь метод public static void printString(String s), который будет выводить переданную строку на экран.
  */
 
 package level_03;
 
 public class task0302 {
-    public static void main(String[] args) {
-        System.out.println(convertEurToUsd(1, 0.86));
-        System.out.println(convertEurToUsd(3, 0.86));
+    public static void printString (String s){
+        System.out.println(s);
     }
-    public static double convertEurToUsd(int eur, double course){
-        return eur * course;
+    public static void main(String[] args) {
+        printString("Hello Amigo!");
     }
 }
 
