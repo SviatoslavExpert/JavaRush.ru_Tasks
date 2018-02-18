19de0a11fbbd3b0bb36181a24f89f848888acb1f
SviatoslavExpert <s.granovskiy@gmail.com>
2018-02-18 15:33:12 +0200

    task0212

---
+++ src/level_02/task0212.java
@@ -0,0 +1,24 @@
+/*
+Великолепная восьмерка
+Создай 10 переменных типа Cat и 8 объектов типа Cat.
+ */
+
+package level_02;
+
+public class task0212 {
+    public static void main (String []args){
+        Cat cat1 = new Cat();
+        Cat cat2 = new Cat();
+        Cat cat3 = new Cat();
+        Cat cat4 = new Cat();
+        Cat cat5 = new Cat();
+        Cat cat6 = new Cat();
+        Cat cat7 = new Cat();
+        Cat cat8 = new Cat();
+        Cat cat9;
+        Cat cat10;
+    }
+
+    public static class Cat {
+    }
+}
