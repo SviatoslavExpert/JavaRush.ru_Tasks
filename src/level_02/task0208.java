efeb5ffb0c49ede6e0ac4af42d6f96b9ee2443da
SviatoslavExpert <s.granovskiy@gmail.com>
2018-02-18 15:18:00 +0200

    task0208

---
+++ src/level_02/task0208.java
@@ -0,0 +1,17 @@
+/*
+Одного кота нам мало
+Создать объект типа Cat 2 раза. Сохрани каждый экземпляр в свою переменную.
+Имена переменных должны быть разные.
+ */
+
+package level_02;
+
+public class task0208 {
+    public static void main (String []args){
+        Cat cat1 = new Cat();
+        Cat cat2 = new Cat();
+    }
+    public static class Cat{
+
+    }
+}
