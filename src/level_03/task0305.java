c75a13e2508b0024a7514824bafaea3ca9fff091
SviatoslavExpert <s.granovskiy@gmail.com>
2018-02-18 21:36:23 +0200

    task0305

---
+++ src/level_03/task0305.java
@@ -0,0 +1,13 @@
+/*
+Я так давно родился
+Вывести на экран дату своего рождения в виде: MAY 1 2012
+ */
+
+package level_03;
+
+public class task0305 {
+    public static void main(String[] args)
+    {
+        System.out.println("MAY 1 1997");
+    }
+}
