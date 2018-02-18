3dad8372556a417849b1c286792842becbc06795
SviatoslavExpert <s.granovskiy@gmail.com>
2018-02-18 14:08:21 +0200

    task0130

---
+++ src/level_02/task0130.java
@@ -0,0 +1,24 @@
+/*
+Наш первый конвертер!
+Метод convertCelsiumToFahrenheit(int celsium) принимает значение в градусах Цельсия. Метод должен переводить температуру и возвращать значение в градусах Фаренгейта.
+Температура по Цельсию TC и температура по Фаренгейту TF связаны следующим соотношением:
+TC = (TF – 32) * 5/9
+Пример:
+В метод convertCelsiumToFahrenheit на вход подается значение 41.
+Пример вывода:
+105.8
+Требования:
+1. Метод convertCelsiumToFahrenheit(int) должен быть публичным и статическим.
+2. Метод convertCelsiumToFahrenheit должен возвращать значение типа double.
+3. Метод convertCelsiumToFahrenheit не должен ничего выводить на экран.
+4. Метод convertCelsiumToFahrenheit должен правильно переводить градусы Цельсия в градусы Фаренгейта и возвращать это число.
+ */
+
+package level_02;
+
+public class task0130 {
+    public static void main(String[] args) {
+        System.out.println(convertCelsiumToFahrenheit(41));
+    }
+    public static double convertCelsiumToFahrenheit(int celsium) { return celsium * 1.8 + 32; }
+}
