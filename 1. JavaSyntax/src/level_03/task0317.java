becdb8897d3a6b352b257449e998f33bc7655e0d
SviatoslavExpert <s.granovskiy@gmail.com>
2018-02-19 10:52:06 +0200

    tasks

---
+++ 1. JavaSyntax/src/level_03/task0317.java
@@ -0,0 +1,12 @@
+/*
+Путь самурая
+Выведи на экран: 日本語
+ */
+
+package level_03;
+
+public class task0317 {
+    public static void main(String[] args) {
+        System.out.print("日本語");
+    }
+}
