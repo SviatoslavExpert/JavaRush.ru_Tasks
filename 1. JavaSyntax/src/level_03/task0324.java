2af6b7c4f61f9c2f35fa0730f02e67a63ddbeddb
SviatoslavExpert <s.granovskiy@gmail.com>
2018-02-20 18:18:53 +0200

    tasks

---
+++ 1. JavaSyntax/src/level_03/task0324.java
@@ -0,0 +1,9 @@
+package level_03;
+
+public class task0324 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++){
+            System.out.println("Я хочу большую зарплату, и для этого изучаю Java");
+        }
+    }
+}
