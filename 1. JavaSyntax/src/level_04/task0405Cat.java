a26d95a8d071243289ce675fc78a0b1bba8b3c3c
Sviatoslav Granovskiy <s.granovskiy@gmail.com>
2018-02-22 09:04:37 +0200

    tasks

---
+++ 1. JavaSyntax/src/level_04/task0405Cat.java
@@ -0,0 +1,17 @@
+/*
+Реализовать метод setCatsCount
+Реализовать метод setCatsCount так, чтобы с его помощью можно
+было устанавливать значение переменной catsCount равное переданному параметру.
+ */
+
+package level_04;
+
+public class task0405Cat {
+    private static int catsCount = 0;
+    public static void setCatsCount(int catsCount) {
+        //напишите тут ваш код
+        task0405Cat.catsCount = catsCount;
+    }
+    public static void main(String[] args) {
+    }
+}
