cda5803453646f457777bc763659987f36790012
SviatoslavExpert <s.granovskiy@gmail.com>
2018-02-23 08:18:50 +0200

    tasks

--- 1. JavaSyntax/src/level_04/task0413.java
+++ 1. JavaSyntax/src/level_04/task0413.java
@@ -11,6 +11,37 @@
 
 package level_04;
 
+import java.io.*;
+
 public class task0413 {
 
+    public static void main(String[] args) throws Exception {
+        BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+        int month = Integer.parseInt(r.readLine());
+        switch (month) {
+            case 1:
+                System.out.println("понедельник");
+                break;
+            case 2:
+                System.out.println("вторник");
+                break;
+            case 3:
+                System.out.println("среда");
+                break;
+            case 4:
+                System.out.println("четверг");
+                break;
+            case 5:
+                System.out.println("пятница");
+                break;
+            case 6:
+                System.out.println("суббота");
+                break;
+            case 7:
+                System.out.println("воскресенье");
+                break;
+            default:
+                System.out.println("такого дня недели не существует");
+        }
+    }
 }
