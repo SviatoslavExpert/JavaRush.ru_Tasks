c37f765fa7a259fa652aa12f595ffa37bd7a7821
SviatoslavExpert <s.granovskiy@gmail.com>
2018-02-24 13:58:28 +0200

    tasks

--- 1. JavaSyntax/src/level_04/task0421.java
+++ 1. JavaSyntax/src/level_04/task0421.java
@@ -6,6 +6,15 @@
  */
 
 package level_04;
+import java.io.*;
 
 public class task0421 {
+    public static void main (String []args) throws Exception {
+        BufferedReader r = new BufferedReader (new InputStreamReader(System.in));
+        String name1 = r.readLine();
+        String name2 = r.readLine();
+
+        if(name1.equals(name2)) System.out.println("Имена идентичны");
+        else if(name1.length() == name2.length()) System.out.println("Длины имен равны");
+    }
 }
