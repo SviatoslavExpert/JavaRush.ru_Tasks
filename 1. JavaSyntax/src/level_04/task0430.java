f49b8fa0f80cea62989f41684a505a617f0d57ca
SviatoslavExpert <s.granovskiy@gmail.com>
2018-02-25 08:26:44 +0200

    tasks

---
+++ 1. JavaSyntax/src/level_04/task0430.java
@@ -0,0 +1,16 @@
+/*
+От 1 до 10
+Вывести на экран числа от 1 до 10 используя цикл while. Каждое значение с новой строки.
+ */
+
+package level_04;
+
+public class task0430 {
+    public static void main (String []args) {
+        int i = 1;
+        while(i < 11){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
