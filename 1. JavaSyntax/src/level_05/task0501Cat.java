941fdf0af48671d85906da5cff76628d3940863a
SviatoslavExpert <s.granovskiy@gmail.com>
2018-02-25 18:58:27 +0200

    tasks

---
+++ 1. JavaSyntax/src/level_05/task0501Cat.java
@@ -0,0 +1,19 @@
+/*
+Создание кота
+Создать класс Cat.
+У кота должно быть имя (name, String), возраст (age, int),
+вес (weight, int), сила (strength, int).
+*/
+
+package level_05;
+
+public class task0501Cat {
+    public String name;
+    public int age;
+    public int weight;
+    public int strength;
+
+    public static void main (String []args){
+
+    }
+}
