d49f5883fb35526d89b90281a293d6a9298fc6ad
SviatoslavExpert <s.granovskiy@gmail.com>
2018-02-25 08:31:19 +0200

    tasks

--- 1. JavaSyntax/src/level_04/task0430.java
+++ 1. JavaSyntax/src/level_04/task0430.java
@@ -9,8 +9,7 @@ public class task0430 {
     public static void main (String []args) {
         int i = 1;
         while(i < 11){
-            System.out.println(i);
-            i++;
+            System.out.println(i++);
         }
     }
 }
