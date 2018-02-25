053dab6f223fd18ebd9238e1bd6b83fc7267ce5d
SviatoslavExpert <s.granovskiy@gmail.com>
2018-02-25 09:10:41 +0200

    tasks

---
+++ 1. JavaSyntax/src/level_04/task0433.java
@@ -0,0 +1,25 @@
+/*
+Гадание на долларовый счет
+Вывести на экран квадрат из 10х10 букв S используя цикл while.
+Буквы в каждой строке не разделять.
+Пример вывода на экран:
+SSSSSSSSSS
+SSSSSSSSSS
+SSSSSSSSSS
+SSSSSSSSSS
+SSSSSSSSSS
+SSSSSSSSSS
+SSSSSSSSSS
+SSSSSSSSSS
+SSSSSSSSSS
+SSSSSSSSSS
+ */
+
+package level_04;
+
+public class task0433 {
+    public static void main (String []args)throws Exception {
+        int i = 0;
+        while(i++ < 10){ System.out.println("SSSSSSSSSS");}
+    }
+}
