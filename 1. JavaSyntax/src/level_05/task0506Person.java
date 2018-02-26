84b969000120713410a2cc7d5f647489476b2508
SviatoslavExpert <s.granovskiy@gmail.com>
2018-02-26 08:51:27 +0200

    tasks

---
+++ 1. JavaSyntax/src/level_05/task0506Person.java
@@ -0,0 +1,19 @@
+/*
+Человечки
+Создать class Person.
+У человека должно быть имя String name, возраст int age,
+адрес String address, пол char sex.
+ */
+
+package level_05;
+
+public class task0506Person {
+    String name;
+    int age;
+    String address;
+    char sex;
+
+    public static void main (String []args) {
+        
+    }
+}
