a31ab1eae500f1f5d63f488dc29b1862f7ff1238
SviatoslavExpert <s.granovskiy@gmail.com>
2018-02-27 12:13:58 +0200

    tasks

--- 1. JavaSyntax/src/level_05/task0516Friend.java
+++ 1. JavaSyntax/src/level_05/task0516Friend.java
@@ -9,17 +9,19 @@
 package level_05;
 
 public class task0516Friend {
-    String name;
-    int age;
-    char sex;
+    private String name;
+    private int age;
+    private char sex;
 
     public void initialize(String name){
         this.name = name;
     }
+
     public void initialize(String name, int age){
         this.name = name;
         this.age = age;
     }
+
     public void initialize(String name, int age, char sex){
         this.name = name;
         this.age = age;
