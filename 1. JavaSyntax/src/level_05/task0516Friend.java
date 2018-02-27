de8c4ce4439ef121028f56c1635af59794f32156
Sviatoslav Granovskiy <s.granovskiy@gmail.com>
2018-02-27 08:58:06 +0200

    tasks

---
+++ 1. JavaSyntax/src/level_05/task0516Friend.java
@@ -0,0 +1,16 @@
+/*
+Друзей не купишь
+Создать класс Friend (друг) с тремя конструкторами:
+- Имя
+- Имя, возраст
+- Имя, возраст, пол
+ */
+
+package level_05;
+
+public class task0516Friend {
+    String name;
+    int age;
+    char sex;
+
+}
