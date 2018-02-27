00b105d430de27fe464e91c3cba6e34b77c56695
Sviatoslav Granovskiy <s.granovskiy@gmail.com>
2018-02-27 09:07:02 +0200

    tasks

--- 1. JavaSyntax/src/level_05/task0516Friend.java
+++ 1. JavaSyntax/src/level_05/task0516Friend.java
@@ -13,4 +13,19 @@ public class task0516Friend {
     int age;
     char sex;
 
+    public void initialize(String name){
+        this.name = name;
+    }
+    public void initialize(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+    public void initialize(String name, int age, char sex){
+        this.name = name;
+        this.age = age;
+        this.sex = sex;
+    }
+
+    public static void main (String []args){
+    }
 }
