19e6774834ecd517fcca3543cf55c4e5cd7cbb24
SviatoslavExpert <s.granovskiy@gmail.com>
2018-02-27 13:20:52 +0200

    tasks

--- 1. JavaSyntax/src/level_05/task0517Cat.java
+++ 1. JavaSyntax/src/level_05/task0517Cat.java
@@ -13,27 +13,37 @@
 package level_05;
 
 public class task0517Cat {
-    private String name, color, address;
-    private int weight, age;
+    private String name = null;
+    private int weight;
+    private int age;
+    private String color;
+    private String address = null;
 
-    public void initialize(String name){
+    public task0517Cat(String name){
         this.name = name;
+        this.weight = 3;
+        this.age = 3;
+        this.color = "white";
     }
-    public void initialize(String name, int weight, int age){
+    public task0517Cat(String name, int weight, int age){
         this.name = name;
-        this.weight = weight;
+        this.weight= weight;
+        this.age = age;
+        this.color = "white";
+    }
+    public task0517Cat(String name, int age){
+        this.name = name;
+        this.weight= 3;
         this.age = age;
     }
-    public void initialize(String name, int age){
-        this.name = name;
-        this.age = age;
-    }
-    public void initialize(int weight, String color){
-        this.weight = weight;
+    public task0517Cat(int weight, String color){
+        this.weight= weight;
+        this.age = 3;
         this.color = color;
     }
-    public void initialize(int weight, String color, String address){
-        this.weight = weight;
+    public task0517Cat(int weight, String color, String address){
+        this.weight= weight;
+        this.age = 3;
         this.color = color;
         this.address = address;
     }
