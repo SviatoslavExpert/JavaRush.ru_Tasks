961ed3df29c01143a86a3480a29206acecba17fe
SviatoslavExpert <s.granovskiy@gmail.com>
2018-02-28 15:08:41 +0200

    tasks

---
+++ 1. JavaSyntax/src/level_05/task0518Dog.java
@@ -0,0 +1,19 @@
+/*
+Регистрируем собачек
+Создать класс Dog (собака) с тремя конструкторами:
+- Имя
+- Имя, рост
+- Имя, рост, цвет
+ */
+
+package level_05;
+
+public class task0518Dog {
+    private String name;
+    private int height;
+    private String color;
+
+    public task0518Dog(String name){
+        this.name = name;
+    }
+}
