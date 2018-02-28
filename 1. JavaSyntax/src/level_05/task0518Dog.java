5cfd866f4b751c38cda89d477eb6bd6d6219ae0a
SviatoslavExpert <s.granovskiy@gmail.com>
2018-02-28 18:42:35 +0200

    tasks

--- 1. JavaSyntax/src/level_05/task0518Dog.java
+++ 1. JavaSyntax/src/level_05/task0518Dog.java
@@ -13,7 +13,20 @@ public class task0518Dog {
     private int height;
     private String color;
 
-    public task0518Dog(String name){
+    public task0518Dog (String name){
         this.name = name;
+    }
+    public task0518Dog (String name, int height) {
+        this.name = name;
+        this.height = height;
+    }
+    public task0518Dog (String name, int height, String color) {
+        this.name = name;
+        this.height = height;
+        this.color = color;
+    }
+
+    public static void main (String []args){
+
     }
 }
