e5c95b8e6bc7a2a5a9404c9333cc1130020ebafb
SviatoslavExpert <s.granovskiy@gmail.com>
2018-03-02 12:32:15 +0200

    tasks

---
+++ 1. JavaSyntax/src/level_06/task0619.java
@@ -0,0 +1,26 @@
+/*
+Добавь 3 public статических переменных: String Solution.name, String Cat.name, String Dog.name.
+
+Требования:
+1. В класс Solution(task0619) добавь public статическую переменную name типа String.
+2. В класс Cat добавь public статическую переменную name типа String.
+3. В класс Dog добавь public статическую переменную name типа String.
+4. В каждом классе должныть своя переменная name.
+ */
+
+package level_06;
+
+public class task0619 {
+    public static String name;
+
+    public static class Cat {
+        public static String name;
+    }
+
+    public static class Dog {
+        public static String name;
+    }
+
+    public static void main(String[] args) {
+    }
+}
