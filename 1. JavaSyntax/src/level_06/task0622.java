3beb603ec512dbca43e298282c4b00414a06c3be
SviatoslavExpert <s.granovskiy@gmail.com>
2018-03-03 18:15:35 +0200

    tasks

--- 1. JavaSyntax/src/level_06/task0622.java
+++ 1. JavaSyntax/src/level_06/task0622.java
@@ -16,5 +16,23 @@
  */
 package level_06;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
 public class task0622 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int[] arr = new int[5];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(reader.readLine());
+        }
+
+        Arrays.sort(arr);
+
+        for (int i : arr) {
+            System.out.println(i);
+        }
+    }
 }
