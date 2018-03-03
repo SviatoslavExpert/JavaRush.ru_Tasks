960abfb1ccbca75c1b7ec6550925d34c4640d94e
SviatoslavExpert <s.granovskiy@gmail.com>
2018-03-03 18:19:45 +0200

    tasks

---
+++ 1. JavaSyntax/src/level_06/task0601Cat.java
@@ -0,0 +1,16 @@
+/*
+Метод finalize класса Cat
+В классе Cat создать метод protected void finalize() throws Throwable.
+ */
+
+package level_06;
+
+public class task0601Cat {
+    protected void finalize() throws Throwable {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
